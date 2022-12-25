8d42b2b2bdd6c3e056597bad9beb4bf1edd40627
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2022-12-25 16:35:06 +0300

    home screen was fixed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:yildiz_motor_v2/backend/theme.dart';
-import 'package:yildiz_motor_v2/screens/add_product_screen.dart';
-import 'package:yildiz_motor_v2/screens/add_supplier_screen.dart';
-import 'package:yildiz_motor_v2/screens/list_product_screen.dart';
-import 'package:yildiz_motor_v2/widgets/menu_button.dart';
+import '../backend/theme.dart';
+import '../widgets/menu_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,91 +23,92 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding:
-            const EdgeInsets.only(left: 300, right: 300, top: 150, bottom: 150),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
+      backgroundColor: YMColors().white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      height: 300,
-                      width: 600,
-                      decoration: const BoxDecoration(),
-                      child: Image.asset(
-                        "assets/images/ym_logo.jpeg",
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Container(
+                        decoration: const BoxDecoration(),
+                        child: Image.asset(
+                          "assets/images/ym_logo.jpeg",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: MenuButton(
+                        text: "Ürünleri Listele",
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, '/list_products');
+                        },
+                        bgColor: YMColors().red,
+                        textColor: YMColors().white,
+                        width: double.infinity,
+                        height: 40,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: MenuButton(
+                        text: "Tedarikçi Listele",
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, '/list_suppliers');
+                        },
+                        bgColor: YMColors().red,
+                        textColor: YMColors().white,
+                        width: double.infinity,
+                        height: 40,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: MenuButton(
+                        text: "Ürün Ekle",
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, '/add_product');
+                        },
+                        bgColor: YMColors().red,
+                        textColor: YMColors().white,
+                        width: double.infinity,
+                        height: 40,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: MenuButton(
+                        text: "Tedarikçi Ekle",
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, '/add_supplier');
+                        },
+                        bgColor: YMColors().red,
+                        textColor: YMColors().white,
+                        width: double.infinity,
+                        height: 40,
                       ),
                     ),
                   ],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    MenuButton(
-                      text: "ÜRÜNLERİ LİSTELE",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, '/list_products');
-                      },
-                      width: 320,
-                      height: 50,
-                      bgColor: YMColors().red,
-                      textColor: YMColors().white,
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    MenuButton(
-                      text: "TEDARİKÇİ LİSTELE",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, '/list_suppliers');
-                      },
-                      bgColor: YMColors().red,
-                      textColor: YMColors().white,
-                      width: 320,
-                      height: 50,
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    MenuButton(
-                      text: "ÜRÜN EKLE",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/add_product');
-                      },
-                      bgColor: YMColors().red,
-                      textColor: YMColors().white,
-                      width: 320,
-                      height: 50,
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    MenuButton(
-                      text: "TEDARİKÇİ EKLE",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, '/add_supplier');
-                      },
-                      bgColor: YMColors().red,
-                      textColor: YMColors().white,
-                      width: 320,
-                      height: 50,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+              ),
+              Expanded(
+                child: Container(),
+              ),
+            ],
+          ),
         ),
       ),
     );
