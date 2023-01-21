acc5b59a4c74825282c1f20543df58c365823d60
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2023-01-21 20:43:16 +0300

    top bar was added to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../backend/theme.dart';
 import '../widgets/custom_button.dart';
+import '../widgets/custom_top_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,103 +11,114 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: YMColors().white,
-      body: Center(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Center(
-            child: Container(
-              width: YMSizes().maxWidth,
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Image.asset(
-                      "assets/images/yildiz_motor_logo.png",
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: CustomButton(
-                      text: "Ürünler",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, '/list_products');
-                      },
-                      textColor: YMColors().white,
-                      bgColor: YMColors().grey,
-                      width: double.infinity,
-                      height: 50,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: CustomButton(
-                      text: "Alımlar",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, '/list_purchases');
-                      },
-                      textColor: YMColors().white,
-                      bgColor: YMColors().grey,
-                      width: double.infinity,
-                      height: 50,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: CustomButton(
-                      text: "Satışlar",
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/list_sales');
-                      },
-                      textColor: YMColors().white,
-                      bgColor: YMColors().grey,
-                      width: double.infinity,
-                      height: 50,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Padding(
+      body: Column(
+        children: [
+          const CustomTopBar(
+            title: 'Ana Sayfa',
+          ),
+          Expanded(
+            child: Center(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Center(
+                  child: Container(
+                    width: YMSizes().maxWidth,
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(30),
+                          child: Image.asset(
+                            "assets/images/yildiz_motor_logo.png",
+                          ),
+                        ),
+                        Padding(
                           padding: const EdgeInsets.all(5),
                           child: CustomButton(
-                            text: "Ayarlar",
+                            text: "Ürünler",
                             onPressed: () {
                               Navigator.pushReplacementNamed(
-                                  context, '/settings');
+                                  context, '/list_products');
                             },
                             textColor: YMColors().white,
-                            bgColor: YMColors().blue,
+                            bgColor: YMColors().grey,
                             width: double.infinity,
                             height: 50,
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Padding(
+                        Padding(
                           padding: const EdgeInsets.all(5),
                           child: CustomButton(
-                            text: "Çıkış Yap",
+                            text: "Alımlar",
                             onPressed: () {
-                              Navigator.pushReplacementNamed(context, '/login');
+                              Navigator.pushReplacementNamed(
+                                  context, '/list_purchases');
                             },
                             textColor: YMColors().white,
-                            bgColor: YMColors().red,
+                            bgColor: YMColors().grey,
                             width: double.infinity,
                             height: 50,
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: CustomButton(
+                            text: "Satışlar",
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(
+                                  context, '/list_sales');
+                            },
+                            textColor: YMColors().white,
+                            bgColor: YMColors().grey,
+                            width: double.infinity,
+                            height: 50,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: CustomButton(
+                                  text: "Ayarlar",
+                                  onPressed: () {
+                                    Navigator.pushReplacementNamed(
+                                        context, '/settings');
+                                  },
+                                  textColor: YMColors().white,
+                                  bgColor: YMColors().blue,
+                                  width: double.infinity,
+                                  height: 50,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: CustomButton(
+                                  text: "Çıkış Yap",
+                                  onPressed: () {
+                                    Navigator.pushReplacementNamed(
+                                        context, '/login');
+                                  },
+                                  textColor: YMColors().white,
+                                  bgColor: YMColors().red,
+                                  width: double.infinity,
+                                  height: 50,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
