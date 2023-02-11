68e4d55b62fbadeb6c41a371be3d78c7880f52c6
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2023-02-11 23:38:45 +0300

    empty dictionaries were added

--- lib/backend/classes.dart
+++ lib/backend/classes.dart
@@ -50,3 +50,45 @@ class Sale {
   String date = "date";
   String formattedDate = "formattedDate";
 }
+
+Map<dynamic, dynamic> emptyCategory = {
+  Category().id: null,
+  Category().name: null,
+};
+
+Map<dynamic, dynamic> emptyProduct = {
+  Product().id: null,
+  Product().name: null,
+  Product().categoryID: null,
+  Product().brand: null,
+  Product().color: null,
+  Product().size: null,
+  Product().sizeType: null,
+  Product().amount: null,
+  Product().price: null,
+  Product().visible: null,
+};
+
+Map<dynamic, dynamic> emptySupplier = {
+  Supplier().id: null,
+  Supplier().name: null,
+  Supplier().phone: null,
+  Supplier().address: null,
+};
+
+Map<dynamic, dynamic> emptyPurchase = {
+  Purchase().id: null,
+  Purchase().supplierID: null,
+  Purchase().productID: null,
+  Purchase().amount: null,
+  Purchase().price: null,
+  Purchase().date: null,
+};
+
+Map<dynamic, dynamic> emptySale = {
+  Sale().id: null,
+  Sale().productID: null,
+  Sale().amount: null,
+  Sale().price: null,
+  Sale().date: null,
+};
