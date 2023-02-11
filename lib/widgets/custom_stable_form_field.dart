88db116dab1e068fc4c48bcfb1e450438c76eb2e
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2023-02-11 14:38:26 +0300

    new stable info box was added

---
+++ lib/widgets/custom_stable_form_field.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../backend/methods.dart';
+import '../backend/theme.dart';
+import 'custom_button.dart';
+
+class CustomStableField extends StatelessWidget {
+  final String? selectionText;
+  final double? width;
+  final double? height;
+  const CustomStableField({
+    super.key,
+    required this.selectionText,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(
+        color: YMColors().white,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: YMColors().grey,
+          width: 2,
+        ),
+      ),
+      child: Text(
+        selectionText ?? "(Yok)",
+        overflow: TextOverflow.visible,
+        textAlign: TextAlign.left,
+        maxLines: 1,
+        style: TextStyle(
+          fontSize: YMSizes().fontSizeSmall,
+          color: (selectionText == null) ? YMColors().grey : YMColors().black,
+        ),
+      ),
+    );
+  }
+}
